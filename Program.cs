2b1fc840cf6dcc6b83ea43fc166bd41609c2cc28
sem-afor <se23m047@technikum-wien.at>
2024-03-18 20:17:20 +0100

    numbers

--- Program.cs
+++ Program.cs
@@ -6,22 +6,25 @@ class Program
 {
     static void Main()
     {
+        // 0 like sequential
+
         // 1000000 bis 1400000
-        //new Quicksort(1000000);
-        //new Quicksort(1050000);
-        //new Quicksort(1100000);
-        //new Quicksort(1150000);
-        //new Quicksort(1200000);
-        //new Quicksort(1250000);
-        //new Quicksort(1300000);
-        //new Quicksort(1350000);
-        //new Quicksort(1400000);
-        //new Quicksort(1450000);
-
+        new Quicksort(1000000,7);
         //new Quicksort(1000000,0);
+        //new Quicksort(1050000,0);
+        //new Quicksort(1100000,0);
+        //new Quicksort(1150000,0);
+        //new Quicksort(1200000,0);
+        //new Quicksort(1250000,0);
+        //new Quicksort(1300000,0);
+        //new Quicksort(1350000,0);
+        //new Quicksort(1400000,0);
+        //new Quicksort(1450000,0);
 
-
-        new Mergesort(4500000, 0);
+        
+        // good nubmers between 10 and 20? check
+        // 4500000 bis 9000000
+        new Mergesort(4500000, 7);
         //new Mergesort(4500000, 0);
         //new Mergesort(4550000,0);
         //new Mergesort(5500000,0);
@@ -36,19 +39,7 @@ class Program
         //new Mergesort(9050000,0);
 
 
-        // 4500000 bis 9000000
-        //new Mergesort(4500000);
-        //new Mergesort(4550000);
-        //new Mergesort(5500000);
-        //new Mergesort(5550000);
-        //new Mergesort(6500000);
-        //new Mergesort(6550000);
-        //new Mergesort(7500000);
-        //new Mergesort(8550000);
-        //new Mergesort(8500000);
-        //new Mergesort(8550000);
-        //new Mergesort(9000000);
-        //new Mergesort(9050000);
+        
 
 
     }
